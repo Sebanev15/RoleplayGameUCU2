2e6106a329abc160fed1e6dca3997d2f4831b9f8
Sebastián Neves <sebastian.neves@correo.ucu.edu.uy>
2025-09-28 11:53:07 -0300

    Refactor Sword and make it an IItem

--- src/Library/Items/Sword.cs
+++ src/Library/Items/Sword.cs
@@ -1,12 +1,18 @@
+using Library.Items;
+
 namespace Ucu.Poo.RoleplayGame;
 
-public class Sword
+public class Sword:IItem
 {
-    public int AttackValue 
+    public int AttackValue { get; set; }
+    public int DefenseValue { get; set; }
+    public bool IsMagical { get; set; }
+
+    public Sword(int attackValue, int defenseValue, bool isMagical)
     {
-        get
-        {
-            return 20;
-        } 
+        this.AttackValue = attackValue;
+        this.DefenseValue = defenseValue;
+        this.IsMagical = isMagical;
+
     }
 }
